071ec83e2f6f006a66d9d2b9edf18d2b64de548b
janekjan <jas.piskorski@spoko.pl>
2020-04-02 23:06:00 +0200

    New void install, new dwm theme

--- bin/dwm/config.h
+++ bin/dwm/config.h
@@ -18,10 +18,22 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+/* Colours form colour picker-generated scheme */
+/*static const char col_green1[]      = "#427d0b";
+static const char col_blue[]        = "#3a55c9";
+static const char col_green2[]      = "#75c926";
+static const char col_orange[]      = "#c93d12";
+static const char col_brown[]       = "#7d2b11";*/
+static const char col_blue[] = "#06a185";
+static const char col_bluegreen[] = "#07ab2a";
+static const char col_green[] = "#4e940d";
+static const char col_grass[] = "#ab9f07";
+static const char col_yellow[] = "#d99d07";
+
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_gray3, col_gray1, col_grass },
+	[SchemeSel]  = { col_gray4, col_green,  col_yellow  },
 };
 
 /* tagging */
@@ -33,7 +45,8 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 }, 
+        { "spotify",  NULL,       NULL,    1<<9,            0,           -1 },
+	{ "Gimp",     NULL,       NULL,       0,            0,           -1 }, 
 	/*	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },*/
 };
 
@@ -63,11 +76,13 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "terminator", NULL };
+static const char *termcmd[]  = { "st", NULL };
+static const char *roficmd[] = { "rofi", "-combi-modi", "window,drun", "-show", "combi", "-modi", "combi", NULL};
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_s,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_s,      spawn,          {.v = roficmd } },
+        { MODKEY,                       XK_r,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
